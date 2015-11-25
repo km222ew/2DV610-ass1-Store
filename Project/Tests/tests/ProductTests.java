3b689110c797f57be4c6f798e4ff3e98b4998296
km222ew <km222ew@student.lnu.se>
2015-11-25 18:58:06 +0100

    Refactor

--- Project/Tests/tests/ProductTests.java
+++ Project/Tests/tests/ProductTests.java
@@ -11,10 +11,13 @@ import static org.mockito.Mockito.*;
 
 public class ProductTests {
 
+	//Writer: Km, Assistance: Hk 
 	@Test(expected = IllegalArgumentException.class)
 	public void ShouldThrowExceptionOnNullNameInput() 
 	{
-		new Product(null, 0);
+		new Product(null, 1);
 	}
+	
+
 
 }
