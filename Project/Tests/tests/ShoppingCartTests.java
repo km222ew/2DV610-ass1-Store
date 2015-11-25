dd95fdeabbc3ee81152230717a18855600262dde
hk222gn <hk222gn@student.lnu.se>
2015-11-25 21:36:16 +0100

    red

--- Project/Tests/tests/ShoppingCartTests.java
+++ Project/Tests/tests/ShoppingCartTests.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import model.Product;
 import model.ShoppingCart;
+import static org.mockito.Mockito.*;
 
 public class ShoppingCartTests {
 
@@ -12,5 +14,13 @@ public class ShoppingCartTests {
 	public void ShouldInitializeProductMap()
 	{
 		assertTrue(new ShoppingCart().GetProducts() != null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void ShouldThrowIllegalArgumentExceptionOnNullInput()
+	{
+		ShoppingCart sc = new ShoppingCart();
+		
+		sc.AddProduct(null, 1);
 	}
 }
