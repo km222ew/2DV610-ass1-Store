2465638a3b368635fe9226bda653cd836647580c
hk222gn <hk222gn@student.lnu.se>
2015-11-25 21:26:26 +0100

    red

---
+++ Project/Tests/tests/ShoppingCartTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.ShoppingCart;
+
+public class ShoppingCartTests {
+
+	@Test
+	public void ShouldInitializeProductMap() 
+	{
+		assertTrue(new ShoppingCart().lhs != null);
+	}
+}
